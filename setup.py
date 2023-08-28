3b04d46cf010bf482ce67f1428189c1d2036e0c5
Kirill <kirill@localhost>
2023-08-28 22:48:49 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='xinetd_http',
+    version='0.1',
+    description='Web gateway interface for using with xinetd',
+    author='Kirill',
+    py_modules=['xinetd_http'],
+)
