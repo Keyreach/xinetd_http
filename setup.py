75b0c0cd094fd9803d63a56c40e8aa02272ee884
Kirill <kirill@localhost>
2023-08-29 12:12:08 +0500

    Rearranged code

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='xinetd_http',
-    version='0.2',
+    version='0.3',
     description='Web gateway interface for using with xinetd',
     author='Kirill',
-    py_modules=['xinetd_http'],
+    packages=['xinetd_http'],
 )
